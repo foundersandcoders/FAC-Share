a8f0740bbd76821b75bf908710ba67c4eca50a42
alinasolinas <azayneeva@gmail.com>
2017-09-07 01:33:11 +0100

    added validation to postData function

--- src/queries/postData.js
+++ src/queries/postData.js
@@ -1,15 +1,36 @@
-const postData = (url, title, dbConnection, cb) => {
-  dbConnection.query(`INSERT INTO resources (url, title) VALUES ($1, $2)`, [url, title], (err, res) => {
+const ifUrlExists = (url, dbConnection, cb) => {
+  dbConnection.query(`SELECT COUNT (*) FROM resources WHERE url='${url}'`, (err, res) => {
     if (err) {
-      cb(err)
-    } else {
-      cb(null, res.rows);
+      console.log("Problem with the database")
+      cb(false)
+    }
+    else {
+      console.log(res.rows)
+      console.log(JSON.stringify(res.rows).includes("0"))
+      if (JSON.stringify(res.rows).includes("0")) {
+        cb(false);
+      } else {
+        cb(true);
+      }
     }
   })
-
-
-  // console.log('inside postData');
-  // console.log('request: ', req.url);
-  // console.log('response :', req.method);
 }
+
+const postData = (url, title, dbConnection, cb) => {
+  ifUrlExists(url, dbConnection, (urlExists) => {
+    if (urlExists) {
+      return console.log("URL already exists")
+    } else {
+      dbConnection.query(`INSERT INTO resources (url, title) VALUES ($1, $2)`, [url, title], (err, res) => {
+        if (err) {
+          cb(err)
+        } else {
+          cb(null, res.rows)
+        }
+      })
+    }
+
+  })
+}
+
 module.exports = postData;
