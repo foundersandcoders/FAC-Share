86f07cb164f3f1a15fbb4680262d2d350d52e960
alinasolinas <azayneeva@gmail.com>
2017-09-08 12:41:17 +0100

    refactored getData, postData, router relates to issue #30

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const postData = require('../queries/postData.js');
+const {postData, ifUrlExists} = require('../queries/postData.js');
 const getData = require('../queries/getData.js');
 const querystring = require('querystring');
-const dbConnection = require('../database/dbConnection.js');
 const sanitizeUrl = require('./sanitizeUrl');
 
 router.get('/get-resource', (req, response) => {
-  getData(dbConnection, (err, res) => {
-    if (err) {
-      console.log('error with getData in router')
-    } else {
-      let output = JSON.stringify(res);
-      response.writeHead(200, {
-        'content-type': 'application/json'
-      });
-      response.end(output);
-    }
-  })
+  getData()
+    .then(res => {
+        let output = JSON.stringify(res.rows);
+        response.writeHead(200, {
+          'content-type': 'application/json'
+        });
+        response.end(output);
+      })
+      .catch(err => {
+        console.log('error with getData in router');
+      })
 })
 
 router.post('/add-resource', (req, res) => {
@@ -26,18 +25,18 @@ router.post('/add-resource', (req, res) => {
 
   const url = querystring.parse(reqUrl)['/add-resource?url'];
   const title = querystring.parse(reqUrl).title;
-
-  console.log("url: " + url);
-  console.log("title: " + title);
-
   const sanitizedUrl = sanitizeUrl(url);
 
-  postData(sanitizedUrl, title, dbConnection, (err, res) => {
-    if (err) {
-      console.log('error');
-    } else {
-      console.log('success');
+  ifUrlExists(sanitizedUrl)
+  .then(response => {
+    if (response.rowCount > 0) throw new Error('URL already exists')
+    else {
+      postData(sanitizedUrl, title)
     }
+  })
+  .then(console.log('success'))
+  .catch(err => {
+    console.log("problem with the database: ", err)
   })
 });
 
