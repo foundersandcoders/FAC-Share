7bdc375286488e405bb5655016fbbb6b1ad185e7
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-09-08 15:30:40 +0100

    chrome extension working Relates #29 #4 #1

---
+++ chrome-extension/content.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    
+    var url = request.url;
+    var title = request.title;
+
+    var finalUrl = 'https://fac-share.herokuapp.com/add-resource' + '?' + 'url=' + url + '&title=' + title;
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else {
+        console.log(xhr.responseType);
+      }
+    }
+    xhr.open("POST", finalUrl, true);
+    xhr.send();
+
+    sendResponse({});
+
+  });
