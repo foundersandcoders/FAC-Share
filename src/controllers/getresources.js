45bd8d754ca766ca2de719baf3786997d557fe4a
rinoma <ganzo.s@libero.it>
2017-09-12 12:19:40 +0100

    removed elements of the main js file

--- src/controllers/getresources.js
+++ src/controllers/getresources.js
@@ -1,15 +1,15 @@
-// const getData = require('../queries/getData.js');
-//
-// module.exports = (req, res) => {
-//   getData()
-//     .then(results => {
-//       let output = JSON.stringify(results.rows);
-//       res.writeHead(200, {
-//         'content-type': 'application/json'
-//       });
-//       res.end(output);
-//     })
-//     .catch(err => {
-//       console.log('Error with getData in router');
-//     })
-// }
+const getData = require('../queries/getData.js');
+
+module.exports = (req, res) => {
+  getData()
+    .then(results => {
+      let output = JSON.stringify(results.rows);
+      res.writeHead(200, {
+        'content-type': 'application/json'
+      });
+      res.end(output);
+    })
+    .catch(err => {
+      console.log('Error with getData in router');
+    })
+}
