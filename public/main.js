982398b0622ce1a214c31ae9d3d21dd36e2f1ee8
rinoma <ganzo.s@libero.it>
2017-09-12 12:21:01 +0100

    removed elements of the main js file

--- public/main.js
+++ public/main.js
@@ -2,37 +2,37 @@ var url = document.getElementById('url');
 var title = document.getElementById('title');
 var submit = document.getElementById('submit');
 
-// (function() {
-//   xhrRequest('/get-resource', renderDom);
-//
-//   function xhrRequest(url, cb) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.onreadystatechange = function() {
-//       if (xhr.readyState === 4 && xhr.status === 200) {
-//         var data = JSON.parse(xhr.responseText);
-//         cb(data);
-//       }
-//     }
-//     xhr.open('GET', url, true);
-//     xhr.send();
-//   }
-//
-//   function renderDom(data) {
-//     var section = document.getElementById('resourceDB')
-//     data.forEach(function(item) {
-//       var point = document.createElement("li");
-//       var newAnchor = document.createElement("a");
-//       newAnchor.textContent = item.title;
-//       newAnchor.setAttribute('href', item.url);
-//       var keywords = document.createElement("p");
-//       keywords.textContent = item.keywords;
-//       point.appendChild(newAnchor);
-//       point.appendChild(keywords);
-//       section.appendChild(point);
-//     })
-//   }
-//
-// })()
+(function() {
+  xhrRequest('/get-resource', renderDom);
+
+  function xhrRequest(url, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        cb(data);
+      }
+    }
+    xhr.open('GET', url, true);
+    xhr.send();
+  }
+
+  function renderDom(data) {
+    var section = document.getElementById('resourceDB')
+    data.forEach(function(item) {
+      var point = document.createElement("li");
+      var newAnchor = document.createElement("a");
+      newAnchor.textContent = item.title;
+      newAnchor.setAttribute('href', item.url);
+      var keywords = document.createElement("p");
+      keywords.textContent = item.keywords;
+      point.appendChild(newAnchor);
+      point.appendChild(keywords);
+      section.appendChild(point);
+    })
+  }
+
+})()
 
 document.getElementById('searchform').addEventListener("submit", function(event) {
   event.preventDefault();
