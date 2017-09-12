2020b193d54ea59eac376de3c2edc60a9e1750cd
rinoma <ganzo.s@libero.it>
2017-09-12 12:13:13 +0100

    Compelted the handlesbars and the loaded results removed comments and console logs

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -3,9 +3,7 @@ const getData = require('../queries/getData.js');
 module.exports = (req, res) => {
   getData()
     .then(results => {
-      // console.log('results: ', results.rows);
       const output = results.rows;
-      // console.log('output: ', output);
       res.render('home', {output})
     })
     .catch(err => {
