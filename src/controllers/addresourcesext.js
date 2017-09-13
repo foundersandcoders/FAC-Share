f1902f2cd669156460eec99a156d33ac81d32772
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-09-13 11:50:17 +0100

    Add keywords functionality to chrome extension Relates #6

--- src/controllers/addresourcesext.js
+++ src/controllers/addresourcesext.js
@@ -5,9 +5,11 @@ module.exports = (req, res, next) => {
 
   const url = querystring.parse(reqUrl)['/add-resource-ext?url'];
   const title = querystring.parse(reqUrl).title;
+  const keywords = querystring.parse(reqUrl).keywords;
 
   req.body.url = url;
   req.body.title = title;
+  req.body.keywords = keywords;
 
   next();
 }
