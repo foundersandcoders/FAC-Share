36d7465e8a68468134731042378ace9d54f8c923
alinasolinas <azayneeva@gmail.com>
2017-09-13 12:06:31 +0100

    added ifInputEmpty function added to backend

--- src/queries/postData.js
+++ src/queries/postData.js
@@ -1,12 +1,23 @@
 const dbConnection = require('../database/dbConnection.js');
 
+const ifInputEmpty = (url, title) => {
+  if (url.replace(/\s+/g, '').length === 0 || title.replace(/\s+/g, '').length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 const ifUrlExists = (url) => {
-    console.log('ifUrlExists: ',  url);
-   return dbConnection.query(`SELECT * FROM resources WHERE url=$1 LIMIT 1`, [url]);
+  return dbConnection.query(`SELECT * FROM resources WHERE url=$1 LIMIT 1`, [url]);
 }
 
 const postData = (url, title, keywords) => {
   return (dbConnection.query(`INSERT INTO resources (url, title, keywords) VALUES ($1, $2, $3)`, [url, title, keywords]))
 }
 
-module.exports = {postData, ifUrlExists};
+module.exports = {
+  postData,
+  ifUrlExists,
+  ifInputEmpty
+};
