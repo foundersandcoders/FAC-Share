7d7881835b9ced80241a048923cd5da095177cd0
rinoma <ganzo.s@libero.it>
2017-09-13 14:17:08 +0100

    Refactored back end validation to be included on extension as well Relates #55

--- src/queries/postData.js
+++ src/queries/postData.js
@@ -1,12 +1,13 @@
 const dbConnection = require('../database/dbConnection.js');
 
-const ifInputEmpty = (url, title) => {
-  if (url.replace(/\s+/g, '').length === 0 || title.replace(/\s+/g, '').length === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// const ifInputEmpty = (url, title, next) => {
+//   if (url.replace(/\s+/g, '').length === 0 || title.replace(/\s+/g, '').length === 0) {
+//     return Promise.resolve('Success');
+//   } else {
+//     return Promise.reject('URL or Title cannot be empty');
+//   }
+//   next();
+// }
 
 const ifUrlExists = (url) => {
   return dbConnection.query(`SELECT * FROM resources WHERE url=$1 LIMIT 1`, [url]);
@@ -19,5 +20,5 @@ const postData = (url, title, keywords) => {
 module.exports = {
   postData,
   ifUrlExists,
-  ifInputEmpty
+  // ifInputEmpty
 };
