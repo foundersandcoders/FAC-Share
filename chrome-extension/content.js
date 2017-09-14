aa64a7b0a46cb5f9d962409a6b8f3c17ccbcb52c
rinoma <ganzo.s@libero.it>
2017-09-14 16:43:13 +0100

    Added a remove/delete functionality for the entries and added helpers for the handlebars Relates #69

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,37 +1,56 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+  function(request, sender, sendResponse) {
 
-      var url = request.url;
-      var title = request.title;
-      var keywords = request.keywords;
+    var url = request.url;
+    var title = request.title;
+    var keywords = request.keywords;
 
-      var finalUrl = 'https://fac-share.herokuapp.com/add-resource-ext' + '?' + 'url=' + url + '&title=' + title + '&keywords=' + keywords;;
+    var finalUrl = 'https://fac-share.herokuapp.com/add-resource-ext' + '?' + 'url=' + url + '&title=' + title + '&keywords=' + keywords;;
 
-      var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          var response = JSON.parse(xhr.responseText)
-          console.log(response);
-          console.log(response.message);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log(xhr.responseText);
+        var response = JSON.parse(xhr.responseText)
+        console.log(response);
+        console.log(response.message);
 
-          console.log('response: ', response);
-          sendResponse(response);
+        console.log('response: ', response);
+        sendResponse(response);
 
-          // if (response) {
-          //   sendResponse(response);
-          //   // var feedback = window.getElementById('feedback');
-          //   // console.log(feedback);
-          //   // feedback.innerHTML(response.message)
-          // } else {
-          //
-          //   console.log(xhr.responseType);
-          // }
-        } else {
-
-        }
-        xhr.open("POST", finalUrl, true);
-        xhr.send();
-
+        // if (response) {
+        //   sendResponse(response);
+        //   // var feedback = window.getElementById('feedback');
+        //   // console.log(feedback);
+        //   // feedback.innerHTML(response.message)
+        // } else {
+        //
+        //   console.log(xhr.responseType);
+        // }
+      } else {
+        console.log(xhr.responseType);
       }
-    });
+      xhr.open("POST", finalUrl, true);
+      xhr.send();
+
+    }
+  }
+)
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var links = document.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    (function() {
+      var ln = links[i];
+      var location = ln.href;
+      ln.onclick = function() {
+        chrome.tabs.create({
+          active: true,
+          url: location
+        });
+      };
+    })();
+  }
+});
