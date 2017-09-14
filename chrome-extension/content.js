568b93329e0a8546aabe203ce03887869e77919e
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-09-14 14:53:17 +0100

    Chrome extension working Relates #4 #6 #29

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,37 +1,23 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+  function(request, sender, sendResponse) {
 
-      var url = request.url;
-      var title = request.title;
-      var keywords = request.keywords;
+    var url = request.url;
+    var title = request.title;
+    var keywords = request.keywords;
 
-      var finalUrl = 'https://fac-share.herokuapp.com/add-resource-ext' + '?' + 'url=' + url + '&title=' + title + '&keywords=' + keywords;;
+    var finalUrl = 'https://fac-share.herokuapp.com/add-resource-ext' + '?' + 'url=' + url + '&title=' + title + '&keywords=' + keywords;;
 
-      var xhr = new XMLHttpRequest();
-
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          var response = JSON.parse(xhr.responseText)
-          console.log(response);
-          console.log(response.message);
-
-          console.log('response: ', response);
-          sendResponse(response);
-
-          // if (response) {
-          //   sendResponse(response);
-          //   // var feedback = window.getElementById('feedback');
-          //   // console.log(feedback);
-          //   // feedback.innerHTML(response.message)
-          // } else {
-          //
-          //   console.log(xhr.responseType);
-          // }
-        } else {
-
-        }
-        xhr.open("POST", finalUrl, true);
-        xhr.send();
-
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else {
+        console.log(xhr.responseType);
       }
-    });
+    }
+    xhr.open("POST", finalUrl, true);
+    xhr.send();
+
+    sendResponse({});
+
+  });
