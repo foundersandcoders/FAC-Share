13ecee1dafa8e928f609561ddc8e19670bc3ed2a
alinasolinas <azayneeva@gmail.com>
2017-09-14 01:07:27 +0100

    added test for getData, postData and searchData functions

--- tests/logic.tests.js
+++ tests/logic.tests.js
@@ -1,4 +1,20 @@
-var test = require('tape');
+var redtape = require('redtape');
+
+var test = redtape({
+  beforeEach: function (cb) {
+    const dbtest = require('./database/test_dbBuild.js')
+    dbtest
+      .then(()=>cb())
+      .catch(cb)
+  },
+  afterEach: function (cb) {
+    const dbtest = require('./database/test_dbBuild.js')
+    dbtest
+      .then(()=>cb())
+      .catch(cb)
+  }
+});
+
 const sanitizeLink = require('../src/controllers/sanitizeUrl.js');
 const addresourcesext = require('../src/controllers/addresourcesext.js');
 const supertest = require('supertest');
@@ -62,25 +78,53 @@ test('sanitizeUrl', (t) => {
 //
 
 //Alina
-// test('getData', (t) => {
-//
-// })
-//
-// test('postData', (t) => {
-//
-// })
-//
-// test('searchData', (t) => {
-//
-// })
-//
-// test('app.js', (t) => {
-//
-// })
-//
-// test('index.js', (t) => {
-//
-// })
+const getData = require('../src/queries/getData.js');
+
+test('getData', (t) => {
+  getData()
+    .then(raw=>{
+      // Checking total count
+      t.equals(raw.rows.length, 2, 'Expect 2 results')
+
+      // Checking first record
+      const firstResult = raw.rows[0]
+      t.equals(firstResult.title, 'Callback HELL', 'Title of the first result should be Callback Hell')
+      t.equals(firstResult.url, 'http://callbackhell.com/', 'Url of the first result should be Callback Hell website')
+
+      t.end()
+    })
+    //when test fails there is no way to see that with catch, that's why we specify t.fail to fail the test in case of a problem
+    .catch(err=>{console.log(err); t.fail(err)})
+})
+
+const { postData } = require('../src/queries/postData.js');
+
+test('postData', (t) => {
+  postData('www.penguin.com', 'Penguin', 'special, bird')
+    .then(res=>{
+      getData()
+        .then(raw=>{
+          const input = raw.rows[2]
+          t.equals(raw.rows.length, 3, 'Expect 1 additional result after insertion')
+          t.equals(input.title, 'Penguin', 'Title of the input resource should be Penguin')
+          t.equals(input.url, 'www.penguin.com', 'Url of the input resource should be penguin.com')
+          t.end()
+        })
+        .catch(err=>{console.log(err); t.fail(err)})
+    })
+    .catch(err=>{console.log(err); t.fail(err)})
+})
+
+const searchData = require('../src/queries/searchData.js');
+
+test('searchData', (t) => {
+  searchData('Async')
+    .then(raw=>{
+      t.equals(raw.rowCount, 1, 'Expect 1 result from searchData')
+      t.end()
+    })
+    .catch(err=>{console.log(err); t.fail(err)})
+})
 
 test('router.js/', (t) => {
   supertest(app)
